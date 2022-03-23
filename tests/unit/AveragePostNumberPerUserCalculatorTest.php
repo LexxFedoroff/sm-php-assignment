f0eae3a36984b2f2f485bc0bb902ad3aca37c6a0
Alexey Fedorov <a.fedorov@city-mobil.ru>
2022-03-23 22:50:44 +0300

    add tests for AveragePostNumberPerUserCalculator

---
+++ tests/unit/AveragePostNumberPerUserCalculatorTest.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\unit;
+
+use PHPUnit\Framework\TestCase;
+use SocialPost\Dto\SocialPostTo;
+use Statistics\Calculator\AveragePostNumberPerUserCalculator;
+use Statistics\Dto\ParamsTo;
+use Statistics\Dto\StatisticsTo;
+
+/**
+ * Class AveragePostNumberPerUserCalculatortTest
+ *
+ * @package Tests\unit
+ */
+class AveragePostNumberPerUserCalculatorTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider getTestCases
+     */
+    public function testCalculate(AveragePostNumberPerUserCalculator $calculator, StatisticsTo $expectedDto): void
+    {
+        $dto = $calculator->calculate();
+        $this->assertDto($expectedDto, $dto);
+    }
+
+    public function getTestCases(): array
+    {
+        return [
+            'empty posts => empty statistic'          =>
+                $this->generateTestCase(d('2022-03-01'), d('2022-03-31'), [], stat([])),
+            'there are no posts in March'             =>
+                $this->generateTestCase(d('2022-03-01'), d('2022-03-31'), $this->generatePosts(), stat([])),
+            'there are 3 authors in January'  =>
+                $this->generateTestCase(d('2022-01-01'), d('2022-01-31'), $this->generatePosts(), stat([
+                    'Author 1' => 2,
+                    'Author 2' => 1,
+                    'Author 3' => 3,
+                ])),
+            'there are 1 author in February' =>
+                $this->generateTestCase(d('2022-02-01'), d('2022-02-28'), $this->generatePosts(), stat([
+                    'Author 2' => 2,
+                ])),
+            'there are 2 authors in from January,20 to February,20' =>
+                $this->generateTestCase(d('2022-01-20'), d('2022-02-20'), $this->generatePosts(), stat([
+                    'Author 3' => 2,
+                    'Author 2' => 2,
+                ]))
+        ];
+    }
+
+    private function assertDto(StatisticsTo $expectedDto, StatisticsTo $actualDto)
+    {
+        $this->assertEquals($expectedDto->getName(), $actualDto->getName());
+        $this->assertEquals($expectedDto->getSplitPeriod(), $actualDto->getSplitPeriod());
+        $this->assertEquals($expectedDto->getUnits(), $actualDto->getUnits());
+        $this->assertEquals($expectedDto->getValue(), $actualDto->getValue());
+
+        $this->assertSameSize($expectedDto->getChildren(), $actualDto->getChildren());
+
+        $n = count($expectedDto->getChildren());
+
+        for ($i = 0; $i < $n; $i++) {
+            $this->assertDto($expectedDto->getChildren()[$i], $actualDto->getChildren()[$i]);
+        }
+    }
+
+    private function generateTestCase(\DateTime $startDate, \DateTime $endDate, iterable $posts, StatisticsTo $expectedStatistics): array
+    {
+        $calculator = new AveragePostNumberPerUserCalculator();
+
+        $params = new ParamsTo();
+        $params->setStatName('test');
+        $params->setStartDate($startDate);
+        $params->setEndDate($endDate);
+        $calculator->setParameters($params);
+
+        foreach ($posts as $post) {
+            $calculator->accumulateData($post);
+        }
+
+        return [$calculator, $expectedStatistics];
+    }
+
+    private function generatePosts(): iterable
+    {
+        yield post('1', '2022-01-01');
+        yield post('1', '2022-01-02');
+        yield post('2', '2022-01-02');
+        yield post('3', '2022-01-02');
+        yield post('3', '2022-01-20');
+        yield post('3', '2022-01-31');
+        yield post('2', '2022-02-01');
+        yield post('2', '2022-02-20');
+    }
+}
+
+function d($date): \DateTime|bool
+{
+    return \DateTime::createFromFormat('Y-m-d', $date);
+}
+
+function stat(array $authors): StatisticsTo
+{
+    $statisticsTo = new StatisticsTo();
+    $statisticsTo->setName('test');
+    $statisticsTo->setUnits('posts');
+
+    foreach ($authors as $name => $amount) {
+        $child = (new StatisticsTo())
+            ->setName('test')
+            ->setSplitPeriod($name)
+            ->setValue($amount)
+            ->setUnits('posts');
+        $statisticsTo->addChild($child);
+    }
+
+    return $statisticsTo;
+}
+
+function post(string $author, string $date): SocialPostTo
+{
+    $post = new  SocialPostTo();
+    $post->setId(uniqid());
+    $post->setAuthorId($author);
+    $post->setAuthorName("Author $author");
+    $post->setText(uniqid());
+    $post->setType('type1');
+    $post->setDate(d($date));
+    return $post;
+}
